5b9d9400436c2a94d6f9dd22c6cf8de1c4457d12
Xavier Amani <xavierzmail05@gmail.com>
2025-03-20 11:55:55 +0300

    NPm test Succesful

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31; 
+const num2 = 2;  
+const multiply = num1 * num2; // 31 * 2 = 62
+const random = Math.ceil(Math.random() * 100); // Random integer between 1-100
+const num3 = 14; 
+const num4 = 5;  
+const mod = num3 % num4; // 14 % 5 = 4
+const max = Math.max(5, 10, 15, 20); // Max is 20
